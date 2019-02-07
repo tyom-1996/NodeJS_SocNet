0376c87719f26beac254e7c7436d51984bec2fb4
hakobjanyanArtyom <dark.artyom1@mail.ru>
2019-02-07 07:42:12 +0400

    voice assistent rename Gideon,and change voice

--- public/js/voice_assistant/voice_assistant.js
+++ public/js/voice_assistant/voice_assistant.js
@@ -1,6 +1,7 @@
 
 
 var audio;
+var speak;
 var finalTranscript = '';
 window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
 var recognition = new window.SpeechRecognition();
@@ -15,76 +16,86 @@ recognition.interimResults = true;
 
 var name = ''
 $(document).ready(function(){
-  setInterval(()=>{
-       // responsiveVoice.speak("команда принята обновляю страницу", "Russian Male", {pitch: 0.9});
-  },2000)
-
+       // // responsiveVoice.speak("команда принята обновляю страницу", "Russian Male", {pitch: 0.8});
   $('body').append('<div class="voice_comand_block"><canvas id="canvas"></canvas></div>');
 })
 
 // recognition.onstart = function() { ... }
 // recognition.onresult = function(event) { ... }
 // recognition.onerror = function(event) { ... }
-// recognition.onaudioend  = function() {
-//   console.log(recognition)
-//   recognition.stop();
-//   console.log('Speech recognition aborted.');
-// }
 
 // recognition.onboundary = function(event) {
 //   console.log( 'Здравствуйте сэр ');
 // }
 
+
+
+function say(text){
+  recognition.stop()
+  speak = new Audio('https://code.responsivevoice.org/getvoice.php?t='+text+'&tl=ru&sv=g1&vn=&pitch=0.4&rate=0.5&vol=1&gender=male');
+  speak.id = 'id'
+  speak.play()
+}
+
 recognition.onresult = (event) => {
 
   let comands = {
 
-      'привет джарвис':()=>{
-          console.log('Джарвис: Здравствуйте сэр')
-        // recognition.stop()
+      'привет гидеон||привет':()=>{
+        console.log('Гидеон: Здравствуйте сэр')
+        say('Здравствуйте сэр')
 
-        voice('Здравствуйте сэр')
 
-      },'джарвис':()=>{
-          console.log('Джарвис: Да сэр')
-        voice('Да сэр')
-      },'послушай джарвис||эй джарвис||послушать джарвис':()=>{
-          console.log('Джарвис: чего тебе')
-      voice('чего тебе')
-      },'джарвис кто ты||кто ты такая||кто ты':()=>{
-          console.log('Джарвис: я онлайн ассистент ваш верный помощник и воображаемый друг')
-        voice('я онлайн ассистент ваш верный помощник и воображаемый друг')
-      },'ты здесь||джарвис ты здесь':()=>{
-          console.log('Джарвис: ну конечно я здесь я же так вас люблю')
-        voice('ну конечно я здесь я же так вас люблю')
-      },'спасибо||джарвис спасибо':()=>{
-          console.log('Джарвис: пожалуйста сэр')
-        voice('пожалуйста сэр')
+      },'гидеон':()=>{
+          console.log('Гидеон: Да сэр')
+        // responsiveVoice.speak('Да сэр',"Russian Male", {pitch: 0.8})
+        say('Да сэр',"Russian Male")
+      },'послушай гидеон||эй гидеон||послушать гидеон':()=>{
+          console.log('Гидеон: чего тебе')
+      // responsiveVoice.speak('чего тебе',"Russian Male", {pitch: 0.8})
+      say('чего тебе',"Russian Male")
+      },'гидеон кто ты||кто ты':()=>{
+          console.log('Гидеон: я онлайн ассистент ваш верный помощник и воображаемый друг ')
+        // responsiveVoice.speak('я онлайн ассистент ваш верный помощник и воображаемый друг',"Russian Male", {pitch: 0.8})
+        say('я онлайн ассистент ваш верный помощник и воображаемый друг я онлайн ассистент ваш верный помощник и воображаемый друг')
+      },'ты здесь||гидеон ты здесь':()=>{
+          console.log('Гидеон: ну конечно я здесь я же так вас люблю')
+        // responsiveVoice.speak('ну конечно я здесь',"Russian Male", {pitch: 0.8})
+        say('ну конечно я здесь',"Russian Male")
+      },'спасибо||гидеон спасибо':()=>{
+          console.log('Гидеон: пожалуйста сэр')
+        // responsiveVoice.speak('пожалуйста сэр',"Russian Male", {pitch: 0.8})
+        say('пожалуйста сэр',"Russian Male")
       },
       'отлично||хорошо':()=>{
-          console.log('Джарвис: Чем могу помоч сэр')
-          voice('Чем могу помоч сэр')
+          console.log('Гидеон: Чем могу помоч сэр')
+          // responsiveVoice.speak('Чем могу помоч сэр',"Russian Male", {pitch: 0.8})
+          say('Чем могу помоч сэр',"Russian Male")
       },
       'профиль||открой профиль||открой страницу профиля||покажи мою страницу||перейди на страницу профиля||открой мою страницу||перейти на страницу профиля': function() {
-        voice('команда принята,выполняют переход')
+        // responsiveVoice.speak('команда принята,выполняют переход',"Russian Male", {pitch: 0.8})
+        say('команда принята,выполняют переход')
         console.log('команда принята,выполняют переход')
         setTimeout(()=>{
           location.href = '/profile'
-        },2000)
+        },3000)
       },
       'новости': function() {
           location.href = '/feed'
       },
-      'как ты||как ты джарвис||как дела||как дела джарвис': function() {
-        console.log('Джарвис:отлично сэр, чем желаете заняться')
-        voice('отлично сэр, чем желаете заняться?')
+      'как ты||как ты гидеон||как дела||как дела гидеон': function() {
+        console.log('Гидеон:отлично сэр, чем желаете заняться')
+        say('отлично сэр, чем желаете заняться?')
+        // // responsiveVoice.speak('отлично сэр, чем желаете заняться?',"Russian Male", {pitch: 0.8})
+        // say('отлично сэр, чем желаете заняться?',"Russian Male", {pitch: 0.8})
       },
       'перезагрузить||обновить||обнови страницу||обновить страницу||перезагрузи страницу': function() {
-        voice('команда принята обновляю страницу')
+        // responsiveVoice.speak('команда принята обновляю страницу',"Russian Male", {pitch: 0.8})
+        say('команда принята обновляю страницу',"Russian Male")
         console.log('команда принята обновляю страницу')
         setTimeout(()=>{
           location.reload()
-        },2000)
+        },3000)
       },
       'открыть запросы': function() {
           if(!$('#requests').hasClass('open'))   $('#requests').addClass('open');
@@ -92,9 +103,10 @@ recognition.onresult = (event) => {
       'закрыть запросы': function() {
           if($('#requests').hasClass('open'))  $('#requests').removeClass('open');
       },
-      'поменяй тему||поменять тему||сменить тему||изменить тему||измени тему||джарвис поменяй тему||джарвис поменять тему||джарвис сменить тему||джарвис изменить тему||джарвис измени тему': function() {
+      'поменяй тему||поменять тему||сменить тему||изменить тему||измени тему||гидеон поменяй тему||гидеон поменять тему||гидеон сменить тему||гидеон изменить тему||гидеон измени тему': function() {
 
-          voice('тема изменена наслаждайтесь')
+          // responsiveVoice.speak('тема изменена',"Russian Male", {pitch: 0.8})
+          say('тема изменена',"Russian Male")
 
           mode = $('#night_mode_toggle').attr('data-mode');
           if ( mode == 'night' ) {
@@ -111,39 +123,46 @@ recognition.onresult = (event) => {
       },
       'отправить запрос||отправь запрос': get_following_request,
       'отменить запрос||отмени запрос':cancel_follow,
-      'включи плеер||плеер||джарвис включи плеер||джарвис загрузи плеер|| загрузи плеер||загрузить плеер':() => {
+      'включи плеер||плеер||гидеон включи плеер||гидеон загрузи плеер|| загрузи плеер||загрузить плеер':() => {
         audio = new Audio('/uploads/upl_music/tenca-aghajanyan-fatum-lev-lev-2018.mp3');
-        console.log('Джарвис: Плеер включён')
-        voice('Плеер включён')
+        console.log('Гидеон: Плеер включён')
+        // responsiveVoice.speak('Плеер включён',"Russian Male", {pitch: 0.8})
+        say('Плеер включён',"Russian Male")
       },
       'включи музыку||загрузи музыку||музыку||воспроизвести трек||продолжай||play||музыка загрузить':() => {
          (audio) ? (
             audio.play(),
-            console.log('Джарвис: Наслаждайтесь сэр'),
-               voice('Наслаждайтесь сэр')
+            console.log('Гидеон: Наслаждайтесь сэр'),
+               // responsiveVoice.speak('Наслаждайтесь сэр',"Russian Male", {pitch: 0.8})
+               say('Наслаждайтесь сэр')
          ) : (
-            console.log('Джарвис: Перед прослушиванием музыки загрузите плеер'),
-               voice('Перед прослушиванием музыки загрузите плеер')
+            console.log('Гидеон: Перед прослушиванием музыки загрузите плеер'),
+               // responsiveVoice.speak('Перед прослушиванием музыки загрузите плеер',"Russian Male", {pitch: 0.8})
+               say('Перед прослушиванием музыки загрузите плеер')
          )
       },
       'поставь на паузу||пауза':() => {
           if (audio) audio.pause();
-          voice('музыка переведена в режим паузы')
+          // responsiveVoice.speak('музыка переведена в режим паузы',"Russian Male", {pitch: 0.8})
+          say('музыка переведена в режим паузы',"Russian Male")
           console.log('музыка переведена в режим паузы')
       },
       'загрузить плеер и включи музыку||загрузи плеер и включи какую-нибудь музыку||включи хорошую музыку':()=>{
         audio = new Audio('/uploads/upl_music/tenca-aghajanyan-fatum-lev-lev-2018.mp3');
         audio.play();
-        voice('хорошо включаю надеюсь вам понравится')
+        // responsiveVoice.speak('хорошо включаю надеюсь вам понравится',"Russian Male", {pitch: 0.8})
+        say('хорошо включаю надеюсь вам понравится',"Russian Male")
 
       },
       'открой новую вкладку||открой новое окно':() => {
           window.open('https://www.google.com/')
-          voice('новая вкладка открыта')
+        // responsiveVoice.speak('новая вкладка открыта',"Russian Male", {pitch: 0.8})
+        say('новая вкладка открыта',"Russian Male")
       },
-      'джарвис включи камеру||включи камеру||включи камеру джарвис':() => {
+      'гидеон включи камеру||включи камеру||включи камеру гидеон':() => {
 
-          voice('камера включена')
+        // responsiveVoice.speak('камера включена',"Russian Male", {pitch: 0.8})
+        say('камера включена',"Russian Male")
         console.log('камера включена')
 
           $('.publisher-box-footer').show()
@@ -171,7 +190,7 @@ recognition.onresult = (event) => {
           console.log('getUserMedia not supported');
         }
       },
-      'выключи камеру||закрой камеру||выключи камеру джарвис':() => {
+      'выключи камеру||закрой камеру||выключи камеру гидеон':() => {
         $('#publisher-camera').val('')
         $('#photo-form').hide()
         $('#image-holder').empty()
@@ -180,10 +199,12 @@ recognition.onresult = (event) => {
         $('.snap_shot').hide()
         $('.close_video_bl').hide()
         webcamStream.stop();
-        voice('камера выключена')
+        // responsiveVoice.speak('камера выключена',"Russian Male", {pitch: 0.8})
+        say('камера выключена',"Russian Male")
       },
       'сделай сэлфи||фото||снимай':() => {
-        voice('Снимок готов,выглядишь прекрасно,так держать')
+        // responsiveVoice.speak('Снимок готов,выглядишь прекрасно',"Russian Male", {pitch: 0.8})
+        say('Снимок готов,выглядишь прекрасно',"Russian Male")
         console.log('выглядишь прекрасно так держать')
           ctx.drawImage(video, 0,0,400,300);
           var base64dataUrl = canvas.toDataURL('image/png');
@@ -205,16 +226,19 @@ recognition.onresult = (event) => {
       },
       'отправь картинку на север||отправить картинку на сервер||отправить данные на сервер||отправь данные на сервер||отправить на сервер':()=>{
           $('#publisher-button').click()
-        voice('данные отправлены на сервер')
+        // responsiveVoice.speak('данные отправлены на сервер',"Russian Male", {pitch: 0.8})
+        say('данные отправлены на сервер',"Russian Male")
       },
-      'открыть консоль||консоль||открой консоль||включи консоль||джарвис открой консоль||джарвис включи консоль':()=>{
+      'открыть консоль||консоль||открой консоль||включи консоль||гидеон открой консоль||гидеон включи консоль':()=>{
         if (!$('.voice_comand_block').hasClass('active_console')){
           $('.voice_comand_block ').addClass('active_console')
-          voice('консоль открыта')
+          // responsiveVoice.speak('консоль открыта',"Russian Male", {pitch: 0.8})
+          say('консоль открыта',"Russian Male")
           console.log('консоль открыта')
 
         }else{
-          voice('будьте внимательнее консоль уже открыто')
+          // responsiveVoice.speak('будьте внимательнее консоль уже открыто',"Russian Male", {pitch: 0.8})
+          say('будьте внимательнее консоль уже открыто',"Russian Male")
           console.log('будьте внимательнее,консоль уже открыто')
         }
       },
@@ -222,35 +246,51 @@ recognition.onresult = (event) => {
         if ($('.voice_comand_block').hasClass('active_console')){
           $('.voice_comand_block ').removeClass('active_console')
           console.log('консоль закрыто до новой встречи')
-          voice('консоль закрыто,До новой встречи')
+          // responsiveVoice.speak('консоль закрыто,До новой встречи',"Russian Male", {pitch: 0.8})
+          say('консоль закрыто,До новой встречи',"Russian Male")
         }else{
           console.log('чтобы что-то закрыт для начала нужно открыть')
-          voice('чтобы что-то закрыт для начала нужно открыть')
+          // responsiveVoice.speak('чтобы что-то закрыт для начала нужно открыть',"Russian Male", {pitch:0.9})
+          say('чтобы что-то закрыт для начала нужно открыть',"Russian Male")
         }
       },
-      'перейти в панель управления||открыть панель управления||панель управления||открой панель управления||джарвис открой панель управления||покажи себя||джарвис покажи себя':()=>{
+      'перейти в панель управления||открыть панель управления||панель управления||открой панель управления||гидеон открой панель управления||покажи себя||гидеон покажи себя':()=>{
         if (location.pathname == '/jarvis-comand-panel' ){
 
           console.log('простите сэр на панели управления уже открыто')
-          voice('простите сэр,но панель управления уже открыто')
+          // responsiveVoice.speak('простите сэр,но панель управления уже открыто',"Russian Male", {pitch: 0.8})
+          say('простите сэр,но панель управления уже открыто',"Russian Male")
         }else{
           location.href = '/jarvis-comand-panel'
           console.log('панель управления открыта')
-          voice('панель управления,открыта.с возвращением')
+          // responsiveVoice.speak('панель управления,открыта.с возвращением',"Russian Male", {pitch: 0.8})
+          say('панель управления,открыта.с возвращением',"Russian Male")
         }
 
       },
-      'пошла к чёрту джарвис||пошла к чёрту':()=>{
+      'пошёл ты к чёрту гидеон||пошёл к чёрту||пошёл ты':()=>{
         console.log('зачем ругаешься начальника')
-        voice('сам,пошёл придурок!')
+        // responsiveVoice.speak('сам,пошёл придурок!',"Russian Male", {pitch: 0.8})
+        say('сам,пошёл придурок!',"Russian Male")
       },
       'почему у тебя такой голос':()=>{
         console.log('Голосок,голосочек,а у тебя мозгов кусочек');
-        voice('Голосок,голосочек,а у тебя мозгов кусочек')
+        // responsiveVoice.speak('Голосок,голосочек,а у тебя мозгов кусочек',"Russian Male", {pitch: 0.8})
+        say('Голосок,голосочек,а у тебя мозгов кусочек')
+        console.log(speak)
       },
-      'а ты шутница':()=>{
+      'а ты шутник':()=>{
           console.log('да я такая')
-          voice('да я такая')
+        // responsiveVoice.speak('да я такой',"Russian Male", {pitch: 0.8})
+        say('да я такой')
+      },
+      'поиск*teg':(teg)=>{
+        if (teg.length > 0){
+          window.open("http://www.google.com/search?q="+ encodeURIComponent(teg))
+          say('найдены следующие результаты по запросу,'+teg)
+        }else{
+          say('команда не выполнена.пожалуйста,введите ключевые слова для поиска')
+        }
       }
   }
 
@@ -274,23 +314,7 @@ function voice_comand(comands){
       let my_comand = transcript;
       console.log('Я:'+my_comand  )
 
-
-      for (let key in comands){
-          let com_arr = key.split('||')
-
-          //dvoynaya komanda
-          if(com_arr.length > 1){
-
-            if (com_arr.indexOf(my_comand) != -1) {
-              comands[key]();
-            }
-
-          //  odinochnaya komanda
-          }else{
-            if (my_comand == key) comands[key]();
-          }
-      }
-
+      command_execution(comands,my_comand);
 
     } else {
       interimTranscript += transcript;
@@ -299,13 +323,91 @@ function voice_comand(comands){
 
   // document.querySelector('body').innerHTML = finalTranscript.toLocaleLowerCase() + '<hr><i style="color:#ddd;">' + interimTranscript + '</>';
   console.log(interimTranscript)
+}
+
+
+
+
+function command_execution(comands,my_comand){
+    for (let key in comands){
+
+    //-----DBOYNAYA KOMANDA------------------
+
+      let two_comands_arr = key.split('||')
+
+      if(two_comands_arr.length > 1){
+          if (two_comands_arr.indexOf(my_comand) != -1) {
+            recognition.stop()
+            comands[key]();
+          }
+      }
+
+    //-----ODINOCHNAYA KOMANDA------------------
+
+      if(my_comand == key){
+          recognition.stop()
+          comands[key]();
+      }
+
+
+    //------SEARCH IN GOOGLE
+
+      else{
+          let search_comands_arr = key.split('*')
+
+          if(search_comands_arr.length > 1){
+              if (my_comand.indexOf(search_comands_arr[0]+' ') != -1){
+                let search_query = my_comand.replace(search_comands_arr[0],'');
+                recognition.stop()
+                comands[key](search_query);
+              }
+          }
+      }
+
+    }
+}
+
+Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
+  get: function(){
+    return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
+  }
+})
+
+recognition.onaudioend = ()=>{
+  // console.log(1)
+  recognition.stop();
+  console.log('micrafon of')
 
 
 }
 
+recognition.onend = ()=>{
 
-recognition.onend = function() {
-  recognition.start()
+
+  var interval = setInterval(()=>{
+    // console.log(isPlaying('id'))
+
+    if (speak){
+      if(speak.playing){ // checks if element is playing right now
+        console.log('play')
+      }else{
+        recognition.start()
+        console.log('micrafon on')
+        clearInterval(interval)
+
+      }
+    }else{
+      recognition.start()
+      console.log('micrafon on')
+      clearInterval(interval)
+    }
+
+  },500)
+
+  // setTimeout(()=>{
+  //
+  //   // console.log(2)
+  // },2000)
 };
 
 // Функция stop для Audio:
@@ -318,7 +420,7 @@ recognition.onend = function() {
 
   var get_following_request = function(){
       if($('.cancel_following_btn').length >  0){
-          console.log('Джарвис:запрос уже отправлен')
+          console.log('Гидеон:запрос уже отправлен')
           return false
       }
       let user_id = $('#user_data').val()
